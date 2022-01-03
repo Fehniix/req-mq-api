d0a137551af65977e08c2549089ed82594187e6a
Fehniix <29045904+Fehniix@users.noreply.github.com>
2022-01-03 11:34:35 +0100

    feat: added module's main index

---
+++ index.ts
@@ -0,0 +1,20 @@
+import IORedis from "ioredis";
+import _SuperRequestable from './lib/SuperRequestable';
+import RequestableClient from './lib/RequestableClient';
+import Method from "./lib/model/Method";
+
+/**
+ * Allows to start a `SuperRequestable` server session.
+ */
+class SuperRequestable {
+	/**
+	 * Starts the SuperRequestable server.
+	 */
+	public start(redis: IORedis.Redis | string): void {
+		_SuperRequestable.start(redis);
+	}
+}
+
+export default new SuperRequestable();
+export { RequestableClient };
+export { Method };
